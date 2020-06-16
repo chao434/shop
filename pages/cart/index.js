34c24d0ea2550e9b85f4108cc812e3ff35292d5a
wlc <wlc@youlove.com.cn>
2020-06-16 13:56:28 +0800

    收货详情修改及优化

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,45 +2,33 @@
 import {getSetting,chooseAddress,openSetting} from "../../utils/syncWX.js"
 import regeneratorRuntime from "../../lib/runtime/runtime.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    address:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+  },
+  onShow(){
+    const address = wx.getStorageSync("address");
+      this.setData({
+        address
+      })
   },
   async handleChoseAddress() {
-    // 获取权限的状态
-    // wx.getSetting({
-    //   success:(result)=>{
-    //     const scopeAddress = result.authSetting["scope.address"];
-    //     if (scopeAddress==true||scopeAddress==undefined){
-    //       wx.chooseAddress({
-    //         success:(result1)=>{
-    //           console.log(result1)
-    //         }
-    //       })
-    //     }else{
-    //       //加入有过拒绝过授予权限的操作,让用户打开授权页面
-    //       wx.openSetting({
-    //         success: (result)=>{
-    //           wx.chooseAddress({
-    //             success:(result1)=>{
-    //               console.log(result1)
-    //             }
-    //           })
-    //         }
-    //       });
-    //     }
-    //   }
-    // })  
-    //获取权限状态
+  try {
+      //获取权限状态
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"];
+      // 判断权限状态
+      if (scopeAddress === false){
+        await openSetting()
+      }
+      let address = await chooseAddress()
+      address.all=address.provinceName+address.cityName+address.countyName+address.detailInfo
+      // 存到缓存中
+      wx.setStorageSync("address",address);
+        
+    } catch (error) {
+      console.log(error);
+    }
   }
 })
