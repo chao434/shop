aea811e5d88014822130b5e9ac7fa738a592705e
wlc <wlc@youlove.com.cn>
2020-06-17 21:00:42 +0800

    购物车代码优化,数量加减

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,5 +1,5 @@
 // pages/cart/index.js
-import {getSetting,chooseAddress,openSetting} from "../../utils/syncWX.js"
+import {getSetting,chooseAddress,openSetting,showModal} from "../../utils/syncWX.js"
 import regeneratorRuntime from "../../lib/runtime/runtime.js"
 Page({
   data:{
@@ -17,27 +17,8 @@ Page({
     const cart = wx.getStorageSync("cart")||[]
     // 计算全选 every 会遍历会接受一个回调函数,只有所有回调值都为true,every的返回值才会是true
     // const allchecked = cart.length? cart.every(item=>item.checked):false;
-    let allchecked = true
-    let totalPrice = 0;
-    let totalNum  = 0;
-    // 遍历计算总价和数量
-    cart.forEach(v => {
-      if (v.checked){
-        totalPrice += v.number*v.goods_price;
-        totalNum += v.number;
-      }else{
-        allchecked = false
-      }
-    });
-    //判断数租是否为空
-    allchecked=cart.length!=0?allchecked:false
-    this.setData({
-      address,
-      cart,
-      allchecked,
-      totalPrice,
-      totalNum
-    })
+    this.setData({address})
+    this.setCart(cart)
   },
   async handleChoseAddress() {
   try {
@@ -63,12 +44,16 @@ Page({
     const goods_id = e.currentTarget.dataset.id
     const {cart} = this.data
     console.log(cart)
+    //获取被修改对象(索引)
     let index = cart.findIndex(v=>v.goods_id===goods_id)
+    // 被修改状态取反
     cart[index].checked=!cart[index].checked
-    this.setData({
-      cart
-    })
-    wx.setStorageSync("cart",cart)
+    //购物车数据计算
+    this.setCart(cart)
+  },
+  //设置购物车状态的同时,重新计算全选总价总数的值
+  
+  setCart(cart){
     let allchecked = true
     let totalPrice = 0;
     let totalNum  = 0;
@@ -89,6 +74,39 @@ Page({
       totalNum,
       allchecked
     })
+    wx.setStorageSync("cart",cart)
   },
-  //设置购物车状态的同时,重新计算全选总价总数的值
+  // 全选框点击事件
+  handleChangAllCheck(){
+    //获取data中的数据
+    let{cart,allchecked} = this.data
+    // 取反
+    allchecked =!allchecked
+    cart.forEach(it=>it.checked=allchecked)
+    this.setCart(cart)
+  },
+  //商品数量的加减操作
+  async handleChangeNumber(e){
+    const {operation,id} = e.currentTarget.dataset
+    //获取购物车数组
+    let {cart} = this.data
+    // 获取被修改对象(索引)
+    let index = cart.findIndex(v=>v.goods_id===id)
+    // 是否要执行删除的操作
+    if(cart[index].number===1&&operation===-1){
+      console.log("/////////////////////")
+      const res = await showModal({content:"你是否要删除"})
+      console.log("/+++++++++++++++++/")
+      if (res.confirm) {
+        console.log("/////////////////////")
+        cart.splice(index,1)
+        this.setCart(cart)
+      }
+    } else {
+      // 购物车数量的加减 
+      cart[index].number+=operation;
+      // 放回data中放回缓存中
+      this.setCart(cart)
+    }
+  }
 })
--- utils/syncWX.js
+++ utils/syncWX.js
@@ -35,3 +35,21 @@ export const openSetting=()=>{
     })
   })
 }
+/**
+ * 
+ * @param {object} param0 
+ */
+export const showModal=({content})=>{
+  return new Promise((resolve,reject)=>{
+    wx.showModal({
+      title: '提示',
+      content: content,
+      success :(res)=>{
+        resolve(res);
+      },
+      fail:(err)=>{
+        reject(err)
+      }
+    })
+  })
+}
