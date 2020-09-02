0a74f198b897200515ad56f82b40b4fce5c7ac70
wlc <wang_liuchao@163.com>
2020-09-02 16:21:10 +0800

    个人中心

--- pages/user/index.js
+++ pages/user/index.js
@@ -5,13 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow (){
+    const userInfo = wx.getStorageSync('userInfo');
+    this.setData({userInfo})
   }
 })
